cd37aeb3bd0775193f40633c529052ab3981cee4
Yuki Omori <yukiomori4211@gmail.com>
2017-11-05 14:33:04 +0900

    待つようになったけどクリック動作しなくなった

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+function sleepByPromise(miliSec) { 
+  return new Promise((resolve) => setTimeout(resolve, miliSec));
+}
+
 function checkAnswer(answerArray, playerArray) {
   let answer = true;
   // ここはeveryで書き換えるべき
@@ -9,45 +13,64 @@ function checkAnswer(answerArray, playerArray) {
   return answer;
 }
 
-function hideAnswer(length, miliSec) {
+async function hideAnswer(length, miliSec) {
   let blankArray = JSON.parse(JSON.stringify((new Array(length)).fill((new Array(length)).fill(''))));
-  setTimeout(generateGrid(blankArray), miliSec);
+  await sleepByPromise(miliSec);
+  generateGrid(blankArray, 'gameBoard');
 }
 
-function generateAnswerArray(length, number) {
+function generateAnswerArray(length, number, diagram) {
   let answerArray = JSON.parse(JSON.stringify((new Array(length)).fill((new Array(length)).fill(''))));
-
+  // numberを使ってランダムに配置
+  let occupied = 0;
+  let col;
+  let row;
+  while(occupied <= number) {
+    row = Math.floor(Math.random() * length);
+    col = Math.floor(Math.random() * length);
+    if (!answerArray[row][col]) {
+      answerArray[row][col] = diagram;
+      occupied++;
+    }
+  }
+  console.log(answerArray);
   return answerArray;
 }
 
-function generateGrid(answerArray) {
-  $('#gameBoard').append(('<tr>' + '<td>●</td>'.repeat(3) + '</tr>').repeat(3));
+function generateGrid(squareArray, boardName) {
+  let tableCode = '<table id="' + boardName + '"><tr>';
+  squareArray.forEach((row, rowNumber) => {
+    row.forEach((element, colNumber) => {
+      tableCode += '<td id="' + rowNumber + colNumber + '">' + element + '</td>';
+    });
+      if (rowNumber === squareArray.length - 1) {
+        tableCode += '</tr>';
+      } else {
+        tableCode += '</tr><tr>';
+      }
+  });
+  $('#gameArea').html(tableCode);
 }
 
 $(() => {
   //情報の受け取り
-  let length = localStorage.getItem('length') ? localStorage.getItem('length') : 3; //マス目の長さ
-  let number = 5; //●の数
+  let length = localStorage.getItem('length') ? JSON.parse(localStorage.getItem('length')) : 3; //マス目の長さ
+  let number = localStorage.getItem('number') ? JSON.parse(localStorage.getItem('number')) : 5; //●の数
   let miliSec = 1000; // 解答が消えるまでの時間
   let diagram = '●'
-  let answerArray = generateAnswerArray(length, number);
+  let answerArray = generateAnswerArray(length, number, diagram);
   let playerArray = JSON.parse(JSON.stringify((new Array(length)).fill((new Array(length)).fill(''))));
 
-  //初期化&描画
-  // $('#gameBoard').append(('<tr>' + '<td>●</td>'.repeat(3) + '</tr>').repeat(3));
-  generateGrid(answerArray);
-
-  // 解答のフェードアウト
-  hideAnswer(length, miliSec);
-
-  // 解答待ち
-    // クリックイベント検知&解答部分に●の表示
   $('td').on('click', function() {
+    let clickedId = $(this).attr('id');
     if ($(this).text()) {
       $(this).text('');
+      playerArray[clickedId[0]][clickedId[1]] = '';
     } else {
+      playerArray[clickedId[0]][clickedId[1]] = diagram;
       $(this).text(diagram);
     }
+    console.log(clickedId, playerArray);
   });
 
   $('#answerButton').on('click', function() {
@@ -64,11 +87,20 @@ $(() => {
 
   $('#nextButton').on('click', function() {
     localStorage.setItem('length', length + 1);
+    localStorage.setItem('number', number + 1);
     location.reload();
   });
 
   $('#retryButton').on('click', function() {
     localStorage.removeItem('length');
+    localStorage.removeItem('number');
     location.reload();
   });
+
+  //初期化&描画
+  generateGrid(answerArray, 'answerBoard');
+
+  // 解答のフェードアウト
+  hideAnswer(length, miliSec);
+
 });
