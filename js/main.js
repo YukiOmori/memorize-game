f00878c0d0dcaca1611dc93df991418c7ba60402
Yuki Omori <yukiomori4211@gmail.com>
2017-11-05 12:54:18 +0900

    add checkAnswer and th on click event

--- js/main.js
+++ js/main.js
@@ -1,50 +1,53 @@
 function checkAnswer(answerArray, playerArray) {
   let answer = true;
   // ここはeveryで書き換えるべき
-  answerArray.forEach(rowNumber, row, () => {
-    row.forEach(colNumber, element, () => {
-      answer = element === playerArray[rowNumber][colNumber] || answer;
+  answerArray.forEach((row, rowNumber) => {
+    row.forEach((element, colNumber) => {
+      answer = element === playerArray[rowNumber][colNumber] && answer;
     });
   });
   return answer;
 }
 
-function flip() {
-  //中身あり ? 表示: 非表示
-}
-
-function hideAnswer() {
-
+function hideAnswer(length, miliSec) {
+  let blankArray = JSON.parse(JSON.stringify((new Array(length)).fill((new Array(length)).fill(''))));
+  setTimeout(generateGrid(blankArray), miliSec);
 }
 
 function generateAnswerArray(length, number) {
-  let answerArray = [];
+  let answerArray = JSON.parse(JSON.stringify((new Array(length)).fill((new Array(length)).fill(''))));
 
   return answerArray;
 }
 
 function generateGrid(answerArray) {
-
+  $('#gameBoard').append(('<tr>' + '<td>●</td>'.repeat(3) + '</tr>').repeat(3));
 }
 
 $(() => {
   //情報の受け取り
   let length = localStorage.getItem('length') ? localStorage.getItem('length') : 3; //マス目の長さ
   let number = 5; //●の数
+  let miliSec = 1000; // 解答が消えるまでの時間
+  let diagram = '●'
   let answerArray = generateAnswerArray(length, number);
-  let playerArray = [];
+  let playerArray = JSON.parse(JSON.stringify((new Array(length)).fill((new Array(length)).fill(''))));
 
   //初期化&描画
-  $('#gameBoard').append(('<tr>' + '<td>●</td>'.repeat(3) + '</tr>').repeat(3));
+  // $('#gameBoard').append(('<tr>' + '<td>●</td>'.repeat(3) + '</tr>').repeat(3));
   generateGrid(answerArray);
 
   // 解答のフェードアウト
-  hideAnswer();
+  hideAnswer(length, miliSec);
 
   // 解答待ち
     // クリックイベント検知&解答部分に●の表示
   $('td').on('click', function() {
-    flip();
+    if ($(this).text()) {
+      $(this).text('');
+    } else {
+      $(this).text(diagram);
+    }
   });
 
   $('#answerButton').on('click', function() {
